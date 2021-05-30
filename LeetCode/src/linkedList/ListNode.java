6e51d276249dfd3bf5f2a8fb326671f85eed543f
Amit Mungare <amungare27@gmail.com>
2021-05-30 21:53:11 +0530

    list node

---
+++ LeetCode/src/linkedList/ListNode.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      public ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  
+	 
+}
