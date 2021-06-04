2a813291b1443e63180fe7dfaf19397734c05d8f
Amit Mungare <amungare27@gmail.com>
2021-06-05 00:16:56 +0530

    delete node in a LL

---
+++ LeetCode/src/linkedList/DeleteNodeinaLinkedList.java
@@ -0,0 +1,10 @@
+package linkedList;
+
+public class DeleteNodeinaLinkedList {
+
+	public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+	
+}
