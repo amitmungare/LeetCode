56768e72e17b136df62344f3510bbb26cfdc710d
Amit Mungare <amungare27@gmail.com>
2021-06-10 22:47:36 +0530

    convert binary number in a LL to int

---
+++ LeetCode/src/linkedList/ConvertBinaryNumberinaLinkedListtoInteger.java
@@ -0,0 +1,25 @@
+package linkedList;
+
+public class ConvertBinaryNumberinaLinkedListtoInteger {
+
+	public int getDecimalValue(ListNode head) {
+        
+        int count =0;
+        ListNode temp =head;
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        int num =count-1;
+        count = 0;
+        
+        while(head != null){
+            if(head.val == 1){
+                count += (int) Math.pow(2,num);
+            }
+            num--;
+            head = head.next;
+        }
+        return count;
+    }
+}
