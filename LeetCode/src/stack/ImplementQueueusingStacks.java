671cca4cb170478fccc1ca93a983fd65afacb742
Amit Mungare <amungare27@gmail.com>
2021-06-11 16:57:49 +0530

    implement Queue using stack

---
+++ LeetCode/src/stack/ImplementQueueusingStacks.java
@@ -0,0 +1,35 @@
+package stack;
+
+import java.util.Stack;
+
+public class ImplementQueueusingStacks {
+	 
+		Stack<Integer> s1 = new Stack();
+	    Stack<Integer> s2= new Stack();
+	    
+
+	    public void push(int x) {
+	       while(!s1.isEmpty()){
+	           s2.push(s1.pop());
+	       }
+	        s1.push(x);
+	       while(!s2.isEmpty()){
+	           s1.push(s2.pop());
+	       }
+	    }
+	    
+	    
+	    public int pop() {
+	        return s1.pop();
+	    }
+	    
+
+	    public int peek() {
+	       return s1.peek();
+	    }
+	    
+
+	    public boolean empty() {
+	       return s1.isEmpty();
+	    }
+}
