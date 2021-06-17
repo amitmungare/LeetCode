e45295a53ae3ffe5409009cf94da8b686b51c039
Amit Mungare <amungare27@gmail.com>
2021-06-17 23:01:22 +0530

    listnode

---
+++ LeetCode/src/bitManipulation/ListNode.java
@@ -0,0 +1,11 @@
+package bitManipulation;
+
+public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      public ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  
+	 
+}
