abe8a0ffe829e79f6a90dab3b0a0cb0e25da32d5
Amit Mungare <amungare27@gmail.com>
2021-06-19 22:10:57 +0530

    length of last word

---
+++ LeetCode/src/string/LengthofLastWord.java
@@ -0,0 +1,21 @@
+package string;
+
+public class LengthofLastWord {
+
+	public int lengthOfLastWord(String s) {
+        
+        int count=0;
+		
+		for(int i=s.length()-1; i>=0; i--) {
+			if(s.charAt(i) !=' ') {
+				count++;
+			}else {
+                if(count>0){
+                    return count;
+                }				
+			}			
+		}
+		return count;
+        
+    }
+}
