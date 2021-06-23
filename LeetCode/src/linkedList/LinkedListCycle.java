9a9d5abf5dc1bd616324f704bb69f02b7c2841e7
Amit Mungare <amungare27@gmail.com>
2021-06-23 23:16:16 +0530

    linked list cycle

---
+++ LeetCode/src/linkedList/LinkedListCycle.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+public class LinkedListCycle {
+	 public boolean hasCycle(ListNode head) {
+	        
+	        if(head==null)return false;
+	        
+	        ListNode slow =head;
+	        ListNode fast =head.next;
+	        
+	        while(slow != fast){
+	            if(fast == null || fast.next == null)return false;            
+	            slow = slow.next;
+	            fast = fast.next.next;
+	        }        
+	        return true;
+	    }
+}
