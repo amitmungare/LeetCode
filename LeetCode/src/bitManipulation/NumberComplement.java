01fbc2ae1823078fa43d79266eb45b8a4356a9ba
Amit Mungare <amungare27@gmail.com>
2021-07-01 17:15:02 +0530

    number complement

---
+++ LeetCode/src/bitManipulation/NumberComplement.java
@@ -0,0 +1,17 @@
+package bitManipulation;
+
+public class NumberComplement {
+
+	public int findComplement(int num) {
+        
+        int i=0;
+        int j=0;
+        
+        while(i<num){
+            i+=Math.pow(2,j);
+            j++;
+        }
+    
+        return i^num;
+    }
+}
