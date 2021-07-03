c7ed59e70f5b947f00857bd3f43405f44a149e8e
Amit Mungare <amungare27@gmail.com>
2021-07-03 23:29:58 +0530

    roman to integer

---
+++ LeetCode/src/string/RomantoInteger.java
@@ -0,0 +1,29 @@
+package string;
+
+import java.util.HashMap;
+
+public class RomantoInteger {
+	 public int romanToInt(String s) {
+	        
+         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		  map.put('I', 1);
+		  map.put('V', 5);
+		  map.put('X', 10);
+		  map.put('L', 50);
+		  map.put('C', 100);
+		  map.put('D', 500);
+		  map.put('M', 1000);
+		 
+		 int result = map.get(s.charAt(s.length()-1));
+				 
+			for(int i=s.length()-2 ; i>=0 ;i--) {
+				if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
+					result -= map.get(s.charAt(i));
+				}else {
+					result += map.get(s.charAt(i));
+				}
+			}
+	       return result;
+        
+    }
+}
